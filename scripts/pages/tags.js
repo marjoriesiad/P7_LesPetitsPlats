2f51ab239f4b7a3faf084e5ed7245427d35d6fe3
Marjorie <marjoriesiad@gmail.com>
2022-06-13 11:52:23 +0200

    affichage des tags ustensiles sans doublons

--- scripts/pages/tags.js
+++ scripts/pages/tags.js
@@ -4,6 +4,8 @@ function getRecipes() {
         .then((data) => {
             const recipes = data.recipes;
             displayIngredients(recipes);
+            displayAppliances(recipes);
+            displayUstensils(recipes);
             selectTags();
         });
 
@@ -27,8 +29,6 @@ function displayIngredients(recipes) {
         })
     })
 
-
-
     let unique = nouveauTableau.filter((el, i) =>
         nouveauTableau.indexOf(el) === i
     );
@@ -37,6 +37,7 @@ function displayIngredients(recipes) {
     unique.forEach(unik => {
         const ingredientList = document.createElement("li");
         ingredientList.classList.add("taglist");
+        ingredientList.classList.add("taglist-ingredients");
         ingredientList.textContent = unik;
         ingredientsSection.appendChild(ingredientList);
     })
@@ -56,8 +57,87 @@ function displayIngredients(recipes) {
         }
 
     });
+}
+
+function displayAppliances(recipes) {
+    const applianceArray = [];
+    const applianceSection = document.querySelector(".appliance-items");
+
+    recipes.forEach((recipe) => {
+            applianceArray.push(recipe.appliance.toLowerCase());
+        })
+        //console.log(applianceArray)
 
 
+    let testArray = [];
+    applianceArray.map(el => {
+        testArray.push(el.toLowerCase());
+    })
+
+    let pouet = testArray.filter((el, i) =>
+        testArray.indexOf(el) === i
+    )
+
+    pouet.forEach(puet => {
+        const applianceList = document.createElement("li");
+        applianceList.classList.add("taglist");
+        applianceList.classList.add("taglist-appliance");
+        applianceList.textContent = puet;
+        applianceSection.appendChild(applianceList);
+    })
+
+    const applianceBtn = document.querySelector(".appliance");
+    const appList = document.querySelector(".appliance-tag");
+
+    applianceBtn.addEventListener("click", () => {
+        appList.classList.toggle("open");
+
+        if (appList.classList.contains("open")) {
+            appList.style.display = "block";
+            //ingredientBtn.style.width = "1000px";
+        } else {
+            appList.style.display = "none";
+        }
+    });
+
+}
+
+function displayUstensils(recipes) {
+    const usenstilsArray = [];
+    const ustenstilsSection = document.querySelector(".ustensils-items");
+
+    recipes.forEach((recipe) => {
+        recipe.ustensils.forEach((ust) => {
+            usenstilsArray.push(ust.toLowerCase());
+        })
+    })
+
+    let ustUnique = usenstilsArray.filter((el, i) =>
+        usenstilsArray.indexOf(el) === i)
+
+
+    //console.log(ustUnique);
+
+    ustUnique.forEach(ustList => {
+        const ustensilList = document.createElement("li");
+        ustensilList.classList.add("taglist");
+        ustensilList.classList.add("taglist-ustensils");
+        ustensilList.textContent = ustList;
+        ustenstilsSection.appendChild(ustensilList);
+    })
+
+    const ustensilsBtn = document.querySelector(".ustensils");
+    const ustensilsList = document.querySelector(".ustensils-tag");
+
+    ustensilsBtn.addEventListener("click", () => {
+        ustensilsList.classList.toggle("open");
+
+        if (ustensilsList.classList.contains("open")) {
+            ustensilsList.style.display = "block";
+        } else {
+            ustensilsList.style.display = "none";
+        }
+    })
 
 }
 
