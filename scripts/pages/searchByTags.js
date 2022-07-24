9e474a880b5c2fcdf87692f7241bdb1b5f19bda8
Marjorie <marjoriesiad@gmail.com>
2022-07-24 16:51:18 +0200

    fix algop de recherche

--- scripts/pages/searchByTags.js
+++ scripts/pages/searchByTags.js
@@ -1,74 +1,95 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
-let arrayFilter = [];
+let ingredientTag = document.querySelectorAll(".taglist-ingredients");
+let applianceTag = document.querySelectorAll(".taglist-appliance");
+let ustensilTag = document.querySelectorAll(".taglist-ustensils");
 
-function searchByTags(recipes) {
+/* Event listener sur l'input recherche */
+document.querySelector(".input").addEventListener("input", (e) => {
+    const searchedString = e.target.value.toLowerCase();
+    searchByTags(recipes, searchedString);
+});
+
+/* searchByTags(recipes, ''); pour event ajout/suppression tag */
+
+/* Fonction de tri de mes recettes */
+function searchByTags(recipes, searchTerms) {
+
+    /* Div des recettes pour affichage */
     const searchResults = document.querySelector(".recipes");
-    arrayFilter = [];
-    if (Array.from(document.querySelectorAll(".tag-selected li")).length === 0) {
-        arrayFilter = recipes;
+
+    /* Initialisation de mon tableau des recettes filtrees */
+    let arrayFilter;
+
+    console.log("Recettes initiales", recipes);
+
+    // filtre recettes à partir de 3 caractères
+    if (searchTerms.length > 2) {
+        const taglist = document.querySelectorAll(".taglist");
+        /* Tri des recettes par le moteur de recherche */
+
+        arrayFilter = recipes.filter(function(recipe) {
+            return recipe.name.toLowerCase().includes(searchTerms);
+        });
+
     } else {
+        /* Attribution de l'ensemble des recettes à arrayFilter */
+        arrayFilter = recipes;
+    }
+
+    /* Verification si tags sélectionner */
+    if (Array.from(document.querySelectorAll(".tag-selected li")).length !== 0) {
+        /* Recuperation de l'ensemble des tags */
         Array.from(document.querySelectorAll(".tag-selected li")).forEach(function(el) {
-            recipes.forEach(recipe => {
+            arrayFilter.forEach(recipe => {
                 if (el.classList.contains("ingredient-selected")) {
                     recipe.ingredients.forEach((ingr) => {
-                        if (ingr.ingredient.toLowerCase() === el.textContent.toLowerCase()) {
-                            if (!arrayFilter.includes(recipe)) {
-                                arrayFilter.push(recipe);
-                            }
+                        if (ingr.ingredient.toLowerCase() !== el.textContent.toLowerCase()) {
+                            arrayFilter.splice(arrayFilter.indexOf(el.textContent), 1);
                         }
                     });
-
                 }
 
                 if (el.classList.contains("appliance-selected")) {
-                    if (recipe.appliance.toLowerCase() === el.textContent.toLowerCase()) {
-                        if (!arrayFilter.includes(recipe)) {
-                            arrayFilter.push(recipe);
-                        }
+                    if (recipe.appliance.toLowerCase() !== el.textContent.toLowerCase()) {
+                        arrayFilter.splice(arrayFilter.indexOf(el.textContent), 1);
                     }
                 }
-
-
 
                 if (el.classList.contains("ustensils-selected")) {
                     recipe.ustensils.filter(item => {
                         if (item.includes(el.textContent.toLowerCase())) {
-                            if (!arrayFilter.includes(recipe)) {
-                                arrayFilter.push(recipe);
-                            }
+
+                        } else {
+                            arrayFilter.splice(arrayFilter.indexOf(el.textContent), 1);
                         }
                     });
                 }
-
-                recipes = arrayFilter;
-
-
-                if (arrayFilter.length === 0) {
-                    searchResults.innerHTML = "Aucune recette trouvée.";
-                }
             });
-
-
-            arrayFilter.splice(1, 1);
-
-            research(arrayFilter);
-
-
-
-            //console.log(arrayFilter);
-
         });
     }
 
 
-    // Affichage des recettes
-    searchResults.innerHTML = "";
-    recipes.forEach((recipe) => {
+    console.log('Tableau filtre par les tags et le moteur de recherche', arrayFilter);
 
-        const recipeModel = recipesFactory(recipe);
-        const recipeCardDOM = recipeModel.getRecipesCardDOM();
-        searchResults.appendChild(recipeCardDOM);
-    });
+    /* si pas de recettes disponibles affichage d'un message d'erreur */
+
+    if (arrayFilter.length === 0) {
+        searchResults.innerHTML = "Aucune recette trouvée.";
+    } else {
+        // Affichage des recettes
+        searchResults.innerHTML = "";
+        arrayFilter.forEach((recipe) => {
+            const recipeModel = recipesFactory(recipe);
+            const recipeCardDOM = recipeModel.getRecipesCardDOM();
+            searchResults.appendChild(recipeCardDOM);
+        });
+    }
+
+
+
+
+
+
 
 }
